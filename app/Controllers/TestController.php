d23a04a05197f2b513497bbc3cfe8d98bb18ed9f
rncs92 <123461096+rncs92@users.noreply.github.com>
2023-09-07 22:15:43 +0300

    Update

--- app/Controllers/TestController.php
+++ app/Controllers/TestController.php
@@ -6,17 +6,17 @@ use Vendon\Core\TwigView;
 
 class TestController
 {
-    public function index()
+    public function index():TwigView
     {
         return new TwigView('Tests/test1', []);
     }
 
-    public function index2()
+    public function index2():TwigView
     {
         return new TwigView('Tests/test2', []);
     }
 
-    public function index3()
+    public function index3():TwigView
     {
         return new TwigView('Tests/test3', []);
     }
